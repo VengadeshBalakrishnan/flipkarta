320b3a826a79eb173982be1af58ee9fb5287f9ef
Vengadesh Balakrishan <vengadesh.balakrishnan@cox.com>
2022-03-08 16:46:57 +0530

    #4 (Add) Binding dyanamic data from Json file in to React app

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+const data = {
+    product: [
+        {
+            _id: '1',
+            name:"Nikie Slim Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: "4.5",
+            numReview: "10",
+            description: "high quality product"
+        },
+        {
+            _id: '2',
+            name:"Medium Shirt",
+            category: "Shirts",
+            image: "/images/p4.jpg",
+            price: 450,
+            brand: "Mike",
+            rating: "3.5",
+            numReview: "100",
+            description: "Medium quality product"
+        },
+        {
+            _id: '3',
+            name:"Cashoo Shirts",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 170,
+            brand: "Bike",
+            rating: "2.5",
+            numReview: "110",
+            description: "quality product"
+        },
+        {
+            _id: '4',
+            name:"Poom Slim Shirt",
+            category: "Shirts",
+            image: "/images/p4.jpg",
+            price: 1558,
+            brand: "Tike",
+            rating: "5",
+            numReview: "1050",
+            description: "high quality product"
+        },
+        {
+            _id: '5',
+            re: "",
+            name:"Tike Slim Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 2320,
+            brand: "Nike",
+            rating: "3.5",
+            numReview: "10",
+            description: "low quality product"
+        },
+        {
+            _id: '6',
+            name:"Adidas Slim Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 4520,
+            brand: "Adidas",
+            rating: "4.5",
+            numReview: "10",
+            description: "high Adidas product"
+        },
+        {
+            _id: '7',
+            name:"Slim Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: "2.5",
+            numReview: "50",
+            description: "quality product"
+        }
+    ]
+}
+
+export default data; 
