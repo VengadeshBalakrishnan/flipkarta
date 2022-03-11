6f80174dde08c6a29438dd441ba61289250a77b4
Vengadesh Balakrishan <vengadesh.balakrishnan@cox.com>
2022-03-11 12:01:01 +0530

    ADD Mongo DB Config - Load data from MONGO DB

--- backend/data.js
+++ backend/data.js
@@ -1,91 +1,100 @@
-const data = {
-    product: [
-        {
-            _id: '1',
-            name:"Nikie Slim Shirt",
-            category: "Shirts",
-            image: "/images/p1.jpg",
-            price: 120,
-            brand: "Nike",
-            rating: "4.5",
-            numReview: "10",
-            description: "high quality product",
-            countInStock: 10
-        },
-        {
-            _id: '2',
-            name:"Medium Shirt",
-            category: "Shirts",
-            image: "/images/p4.jpg",
-            price: 450,
-            brand: "Mike",
-            rating: "3.5",
-            numReview: "100",
-            description: "Medium quality product",
-            countInStock: 20
-        },
-        {
-            _id: '3',
-            name:"Cashoo Shirts",
-            category: "Shirts",
-            image: "/images/p1.jpg",
-            price: 170,
-            brand: "Bike",
-            rating: "2.5",
-            numReview: "110",
-            description: "quality product",            
-            countInStock: 0
-        },
-        {
-            _id: '4',
-            name:"Poom Slim Shirt",
-            category: "Shirts",
-            image: "/images/p4.jpg",
-            price: 1558,
-            brand: "Tike",
-            rating: "5",
-            numReview: "1050",
-            description: "high quality product",
-            countInStock: 30
-        },
-        {
-            _id: '5',
-            re: "",
-            name:"Tike Slim Shirt",
-            category: "Shirts",
-            image: "/images/p1.jpg",
-            price: 2320,
-            brand: "Nike",
-            rating: "3.5",
-            numReview: "10",
-            description: "low quality product",
-            countInStock: 4
-        },
-        {
-            _id: '6',
-            name:"Adidas Slim Shirt",
-            category: "Shirts",
-            image: "/images/p1.jpg",
-            price: 4520,
-            brand: "Adidas",
-            rating: "4.5",
-            numReview: "10",
-            description: "high Adidas product"     ,
-            countInStock: 100
-        },
-        {
-            _id: '7',
-            name:"Slim Shirt",
-            category: "Shirts",
-            image: "/images/p1.jpg",
-            price: 120,
-            brand: "Nike",
-            rating: "2.5",
-            numReview: "50",
-            description: "quality product",            
-            countInStock: 0
-        }
-    ]
-}
+import bcrypt from "bcrypt";
 
-export default data; 
+const data = {
+  users: [
+    {
+      name: "Vengadesh",
+      email: "vengadesh@test.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "testuser",
+      email: "test@test.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+  product: [
+    {     
+      name: "Nikie Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: "4.5",
+      numReviews: "10",
+      description: "high quality product",
+      countInStock: 10,
+    },
+    {    
+      name: "Medium Shirt",
+      category: "Shirts",
+      image: "/images/p4.jpg",
+      price: 450,
+      brand: "Mike",
+      rating: "3.5",
+      numReviews: "100",
+      description: "Medium quality product",
+      countInStock: 20,
+    },
+    {     
+      name: "Cashoo Shirts",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 170,
+      brand: "Bike",
+      rating: "2.5",
+      numReviews: "110",
+      description: "quality product",
+      countInStock: 0,
+    },
+    {      
+      name: "Poom Slim Shirt",
+      category: "Shirts",
+      image: "/images/p4.jpg",
+      price: 1558,
+      brand: "Tike",
+      rating: "5",
+      numReviews: "1050",
+      description: "high quality product",
+      countInStock: 30,
+    },
+    {      
+      re: "",
+      name: "Tike Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 2320,
+      brand: "Nike",
+      rating: "3.5",
+      numReviews: "10",
+      description: "low quality product",
+      countInStock: 4,
+    },
+    {
+      name: "Adidas Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 4520,
+      brand: "Adidas",
+      rating: "4.5",
+      numReviews: "10",
+      description: "high Adidas product",
+      countInStock: 100,
+    },
+    {      
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: "2.5",
+      numReviews: "50",
+      description: "quality product",
+      countInStock: 0,
+    },
+  ],
+};
+
+export default data;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,38 @@
 import express from "express";
-import data from "./data.js";
+import mongoose from "mongoose";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/flipkarta', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.get("/", (req, res) => {
   res.send("Server Started");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.product);
-});
+// app.get("/api/products", (req, res) => {
+//   res.send(data.product);
+// });
 
-app.get("/api/products/:id", (req, res) => {
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
-  let product = data.product.find((a) => a._id === req.params.id);
+// app.get("/api/products/:id", (req, res) => {
+//   let product = data.product.find((a) => a._id === req.params.id);
 
-  if (product) {
-    console.log('test', product)
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
+//   if (product) {
+//     console.log("test", product);
+//     res.send(product);
+//   } else {
+//     res.status(404).send({ message: "Product Not Found" });
+//   }
+// });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
